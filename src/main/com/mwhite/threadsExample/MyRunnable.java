5889ba3896c21a304512fbaddd3d203e9855a1aa
Mary White <mary.a.white28@gmail.com>
2022-11-28 21:45:19 -0500

    restructured files, wrote unit tests for rewardPoints class

--- src/main/com/mwhite/threadsExample/MyRunnable.java
+++ src/main/com/mwhite/threadsExample/MyRunnable.java
@@ -2,11 +2,9 @@ package com.mwhite.threadsExample;
 
 //create a class that implements Runnable
 
-import static com.mwhite.threadsExample.ThreadColor.ANSI_RED;
-
 public class MyRunnable implements Runnable{
     @Override
     public void run() {
-        System.out.println(ANSI_RED + "Hello from Runnable.");
+        System.out.println(ThreadColor.ANSI_RED + "Hello from Runnable.");
     }
 }
